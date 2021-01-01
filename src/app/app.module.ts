e11c4bc93030f4ec420fd5d5b17491549002b128
rishko00 <vasylrishko21@gmail.com>
2021-01-01 15:54:26 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza.component';
 import { BasketComponent } from './basket.component';
@@ -20,6 +21,7 @@ const appRoutes: Routes =[
     AppComponent, PizzaComponent, BasketComponent, ConstructorComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes)
--- src/app/basket.component.ts
+++ src/app/basket.component.ts
@@ -1,6 +1,75 @@
 import { Component } from '@angular/core';
 import { BasketService } from './basket.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+
+class ToOrder{
+  items: Order[];
+  totalPrice: number;
+  user: string;
+  address: string;
+  phoneNumber: string;
+  date: Date;
+
+  constructor(items, user, address, phoneNumber, date){
+    this.address = address;
+    this.items = items;
+    this.user = user;
+    this.phoneNumber = phoneNumber;
+    this.date = date;
+    this.totalPrice = this.getTotalPrice();
+  }
+
+  getTotalPrice(){
+    let sum = 0;
+    for(let i of this.items){
+      sum += i['totalPrice'];
+    }
+    return sum;
+  }
+}
+
+class Pizza{
+  name: string;
+  info: string;
+  price: Object;
+  image: string;
+  defaultSize: string;
+  ingredients: Ingredient[];
+
+  constructor(name, info, price, image, ingredients){
+    this.name = name;
+    this.info = info;
+    this.image = image;
+    this.price = price;
+    this.ingredients = ingredients;
+  }
+}
+
+class Ingredient{
+  name: string;
+  price: number;
+  image: string;
+
+  constructor(name, price, image){
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+}
+
+class Order{
+  item: Pizza;
+  totalPrice: number;
+  count: number;
+
+  constructor(item, totalPrice, count){
+    this.item = item;
+    this.totalPrice = totalPrice;
+    this.count = count;
+  }
+}
 
 @Component({
   selector: 'basket',
@@ -11,7 +80,12 @@ import { BasketService } from './basket.service';
   <span *ngFor="let j of i.item.ingredients"> {{ j.name }}</span><br>
   {{ i.totalPrice }}<br></span></div>
   <div>Сума замовлення: {{getTotalPrice()}}</div>
-  <button (click)="toOrder()">OK</button>`,
+  
+    User: <input [(ngModel)]="user">
+    Address: <input [(ngModel)]="address">
+    Number: <input [(ngModel)]="phoneNumber">
+    <button (click)="toOrder()">OK</button>
+  `,
   styles: [`
   img{
     width: 200px;
@@ -21,8 +95,12 @@ import { BasketService } from './basket.service';
 
 export class BasketComponent {
   basketItems: Object[] = this.basket.getItems();
+  user: string;
+  address: string;
+  phoneNumber: string;
+  date: Date = new Date();
 
-  constructor(private basket: BasketService){ }
+  constructor(private basket: BasketService, private http: HttpClient){ }
 
   getTotalPrice(){
     let sum = 0;
@@ -32,5 +110,20 @@ export class BasketComponent {
     return sum;
   }
 
-  toOrder(){}
+  toOrder() {
+    let order = new ToOrder(this.basketItems, this.user, this.address, this.phoneNumber, this.date);
+
+    this.http.post('https://pizzeria-ec9c3-default-rtdb.europe-west1.firebasedatabase.app/Orders.json', o) .subscribe(
+        (val) => {
+            console.log("POST call successful value returned in body", 
+                        val);
+        },
+        response => {
+            console.log("POST call in error", response);
+        },
+        () => {
+            console.log("The POST observable is now completed.");
+        });
+    console.log(JSON.stringify(order))
+  }
 }
