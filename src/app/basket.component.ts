29deadbeb46762c8683f17f7a0b55ec0d20a86b1
rishko00 <vasylrishko21@gmail.com>
2021-01-15 16:09:19 +0200

    2 files modified

--- src/app/basket.component.ts
+++ src/app/basket.component.ts
@@ -1,75 +1,8 @@
 import { Component, ElementRef } from '@angular/core';
 import { BasketService } from './basket.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule, NgModel, NgForm, FormGroup, FormControl, Validators  } from '@angular/forms';
-
-
-class ToOrder{
-  items: Order[];
-  totalPrice: number;
-  user: string;
-  address: string;
-  phoneNumber: string;
-  date: Date;
-
-  constructor(items, user, address, phoneNumber, date){
-    this.address = address;
-    this.items = items;
-    this.user = user;
-    this.phoneNumber = phoneNumber;
-    this.date = date;
-    this.totalPrice = this.getTotalPrice();
-  }
-
-  getTotalPrice(){
-    let sum = 0;
-    for(let i of this.items){
-      sum += i['totalPrice'];
-    }
-    return sum;
-  }
-}
-
-class Pizza{
-  name: string;
-  info: string;
-  price: Object;
-  image: string;
-  defaultSize: string;
-  ingredients: Ingredient[];
-
-  constructor(name, info, price, image, ingredients){
-    this.name = name;
-    this.info = info;
-    this.image = image;
-    this.price = price;
-    this.ingredients = ingredients;
-  }
-}
-
-class Ingredient{
-  name: string;
-  price: number;
-  image: string;
-
-  constructor(name, price, image){
-    this.name = name;
-    this.price = price;
-    this.image = image;
-  }
-}
-
-class Order{
-  item: Pizza;
-  totalPrice: number;
-  count: number;
-
-  constructor(item, totalPrice, count){
-    this.item = item;
-    this.totalPrice = totalPrice;
-    this.count = count;
-  }
-}
+import { BasketItem, Order } from './models';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'basket',
@@ -101,27 +34,17 @@ export class BasketComponent {
   }
 
   toOrder() {
-    let order = new ToOrder(this.basketItems, this.user, this.address, this.phoneNumber, new Date());
-
-    this.http.post('https://pizzeria-ec9c3-default-rtdb.europe-west1.firebasedatabase.app/Orders.json', order) .subscribe(
-        (val) => {
-            console.log("POST call successful value returned in body", 
-                        val);
-        },
-        response => {
-            console.log("POST call in error", response);
-        },
-        () => {
-            console.log("The POST observable is now completed.");
-        });
-    console.log(JSON.stringify(order))
+    let order = new Order(this.basket.getItems(), this.user, this.address, this.phoneNumber, new Date());
+    this.http.post('https://pizzeria-ec9c3-default-rtdb.europe-west1.firebasedatabase.app/Orders.json', order) .subscribe((val) => {
+      console.log("POST call successful value returned in body", val);
+    });
   }
 
-  deleteOrder(o: Order){
+  deleteOrder(o: BasketItem){
     this.basket.delete(o);
   }
 
-  incrOrder(o: Order, act: string){
+  incrOrder(o: BasketItem, act: string){
     this.basket.incOrder(o, act);
   }
 }
