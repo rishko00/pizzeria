b358e4ba14501ce909771ed57a4ba37a21c3992d
rishko00 <vasylrishko21@gmail.com>
2021-01-16 15:19:08 +0200

    1 file modified

--- src/app/auth-admin.component.ts
+++ src/app/auth-admin.component.ts
@@ -4,30 +4,20 @@ import { AuthService } from './auth.service'
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container pt-3">
-  <div class="row justify-content-sm-center">
-    <div class="col-sm-10 col-md-6">
-      <div class="card border-info">
-        <div class="card-header">Login</div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-4 text-center">
-              <img src="https://placeimg.com/128/128/nature">
-            </div>
-            <div class="col-md-8">
-              <form>
-                <input type="text" class="form-control mb-2" placeholder="Email" [(ngModel)]="email" name="email" required autofocus>
-                <input type="password" class="form-control mb-2" placeholder="Password" [(ngModel)]="password" name="password" required>
-                <button class="btn btn-lg btn-primary btn-block mb-1" type="submit" (click)="login()">Login</button>
-              </form>
-            </div>
-          </div>
-        </div>
+  <div class="form-group">
+        <label>Email:</label>
+        <input class="form-control" type="text" name="email" [(ngModel)]="email"/>
       </div>
-    </div>
-  </div>
-</div>`,
-  styles: ['./app.component.css']
+
+      <div class="form-group">
+        <label for="Autocomplete">Password</label>
+        <input type="text" class="form-control" name="email" [(ngModel)]="email"/>
+      </div>
+      <button class="orderbtn" (click)="login()">Оформити замовлення</button>
+      <div class="alert alert-danger" role="alert" *ngIf="login()">
+  A simple danger alert—check it out!
+</div>
+  `
 })
 
 export class AuthComponent {
@@ -39,6 +29,8 @@ export class AuthComponent {
   ngOnInit(){};
 
   login(){
-    this.auth.login(this.email, this.password)
+    this.auth.login(this.email, this.password);
+    if(localStorage.getItem('user')) return true;
+    else return fale
   }
 }
