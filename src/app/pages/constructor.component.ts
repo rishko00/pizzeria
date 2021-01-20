c1948b657d370fe4306103ee9a605b803d1cc846
rishko00 <43280707+rishko00@users.noreply.github.com>
2021-01-20 11:35:03 +0200

    Create constructor.component.ts

---
+++ src/app/pages/constructor.component.ts
@@ -0,0 +1,28 @@
+import { Component, Renderer2 } from '@angular/core';	
+import { DataService } from './data.service';	
+import { Pizza, Ingredient, BasketItem, PizzaBase } from './models';	
+import { BasketService } from './basket.service';	
+
+@Component({	
+  selector: 'constructor',	
+  templateUrl: './constructor.component.html',	
+  styleUrls: ['./constructor.component.css']	
+})	
+
+export class ConstructorComponent {	
+  ingredients: Ingredient[] = [];	
+  pizzaBase: PizzaBase[] = [];	
+  pizza: Pizza = new Pizza('Ваш шедевр','https://cdn10.arora.pro/f/upload/f81d1064-1337-4bbf-a894-909133be0aa2/file_manager/theme/no-photo-small.jpg', {}, '');	
+  order: BasketItem = new BasketItem(this.pizza, 0, 1);	
+  z: number = 0;	
+
+  constructor(private data: DataService, private basket: BasketService, private renderer: Renderer2){ }	
+
+  ngOnInit(){	
+    this.data.getIngredients().subscribe((data) => {	
+      for(let i of data){	
+        this.ingredients.push(new Ingredient(i.name, i.price, i.image, i.constructorImage));	
+      }	
+    });	
+  }
+}
